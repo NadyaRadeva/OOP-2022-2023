440d4d2d9b144c1c579cd4a0fedcbdce4d6e54be
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2023-03-14 01:31:42 +0200

    Create Array.cpp

---
+++ Homework for seminars/Homework 3/Array.cpp
@@ -0,0 +1,139 @@
+#include "Array.h"
+#include <iostream>
+#include <cassert>
+
+void Array::allocateMemory(int*& array, size_t capacity)
+{
+    assert(array == nullptr);
+
+    array = new int[capacity];
+}
+
+void Array::freeMemory(int*& array, size_t& size, size_t& capacity)
+{
+    assert(array != nullptr);
+
+    delete[] array;
+    array = nullptr;
+    size = 0;
+    capacity = 0;
+}
+
+bool Array::reallocateMemory(int*& array, size_t size, size_t newCapacity)
+{
+    assert(array != nullptr);
+
+    int* tempArray = new (std::nothrow) int[newCapacity];
+
+    if (tempArray == nullptr)
+    {
+        return false;
+    }
+
+    for (size_t i = 0; i < size; i++)
+    {
+        tempArray[i] = array[i];
+    }
+
+    delete[] array;
+    array = tempArray;
+    return true;
+}
+
+bool Array::resize(int*& array, size_t size, size_t& capacity)
+{
+    assert(array != nullptr);
+
+    if (size < capacity)
+    {
+        return true;
+    }
+    capacity = capacity * 2;
+    return reallocateMemory(array, size, capacity);
+}
+
+bool Array::addElement(int*& array, size_t& size, size_t& capacity, int newElem)
+{
+    assert(array != nullptr);
+
+    if (size == capacity)
+    {
+        if (!resize(array, size, capacity))
+        {
+            return false;
+        }
+    }
+    array[size++] = newElem;
+    return true;
+}
+
+bool Array::addElement(int*& array, size_t& size, size_t& capacity, int newElem, size_t position)
+{
+    assert(array != nullptr);
+
+    if (position > size)
+    {
+        return false;
+    }
+    if (position == size)
+    {
+        return addElement(array, size, capacity, newElem);
+    }
+
+    if (size == capacity)
+    {
+        if (!resize(array, size, capacity))
+        {
+            return false;
+        }
+    }
+
+    for (size_t i = size; i > position; i--)
+    {
+        array[i] = array[i - 1];
+    }
+
+    array[position] = newElem;
+    size++;
+    return true;
+}
+
+void Array::print(const int* array, size_t size, size_t capacity) const
+{
+    assert(array != nullptr);
+
+    std::cout << "Array contents: ";
+    for (size_t i = 0; i < size; i++)
+    {
+        std::cout << array[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+bool Array::removeElement(int*& array, size_t& size, size_t& capacity, size_t position)
+{
+    assert(array != nullptr);
+
+    if (position >= size)
+    {
+        return false;
+    }
+    for (size_t i = position; i < size - 1; i++)
+    {
+        array[i] = array[i + 1];
+    }
+
+    size--;
+
+    if (size * 4 < capacity)
+    {
+        size_t newCapacity = capacity / 2;
+        if (!reallocateMemory(array, size, newCapacity))
+        {
+            return false;
+        }
+        capacity = newCapacity;
+    }
+
+    return true;
+}
