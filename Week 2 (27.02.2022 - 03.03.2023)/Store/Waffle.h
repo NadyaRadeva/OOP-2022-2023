9ae107bea5a372bb2bb60ae5061ba86a6284e6ba
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2023-03-14 01:19:15 +0200

    Create Waffle.h

---
+++ Week 2 (27.02.2022 - 03.03.2023)/Store/Waffle.h
@@ -0,0 +1,14 @@
+#ifndef WAFFLE_H_
+#define WAFFLE_H_
+
+#include<cstring>
+const unsigned MAX_SIZE = 21;
+
+class Waffle {
+	char brand[MAX_SIZE];
+	double weight;
+	double priceForMany;
+	double priceForAClient;
+}
+
+#endif // !
