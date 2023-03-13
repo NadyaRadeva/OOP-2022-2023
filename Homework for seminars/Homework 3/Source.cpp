a0809ec30c40a86bf4098e11488a05798d3372e4
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2023-03-14 01:33:17 +0200

    Update Source.cpp

--- Homework for seminars/Homework 3/Source.cpp
+++ Homework for seminars/Homework 3/Source.cpp
@@ -1,1 +1,39 @@
+#include "Array.h"
+#include <cassert>
+#include <iostream>
 
+int main()
+{
+    int *array{nullptr};
+
+    size_t capacity = 3;
+
+    size_t size = 0;
+
+    allocateMemory(array, capacity);
+
+    addElement(array, size, capacity, 1);
+    addElement(array, size, capacity, 2);
+    addElement(array, size, capacity, 3);
+
+    std::cout << "The capacity of the array is " << capacity << std::endl;
+    std::cout << "The real count of the elements in the array is " << size << std::endl;
+    print(array, size, capacity);
+
+    addElement(array, size, capacity, 4, 2);
+
+    std::cout << "\nThe capacity of the array is " << capacity << std::endl;
+    std::cout << "The real count of the elements in the array is " << size << std::endl;
+    print(array, size, capacity);
+
+    removeElement(array, size, capacity, 0);
+    removeElement(array, size, capacity, 0);
+    removeElement(array, size, capacity, 0);
+
+    std::cout << "\nThe capacity of the array is " << capacity << std::endl;
+    std::cout << "The real count of the elements in the array is " << size << std::endl;
+
+    print(array, size, capacity);
+
+    freeMemory(array, size, capacity);
+}
