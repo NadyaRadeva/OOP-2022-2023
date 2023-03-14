74c61065f041d8a11ae3c6cbdae555854fc7ec9f
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2023-03-14 11:34:09 +0200

    Create Matrix.h

---
+++ Week 4 (13.03.2023 - 19.03.2023)/Matrix.h
@@ -0,0 +1,23 @@
+#ifndef _MATRIX_H
+#define _MATRIX_H
+
+class Matrix {
+private:
+	unsigned cols, rows;
+
+public:
+	Matrix(unsigned r, unsigned c);
+	~Matrix();
+	Matrix& operator=(const Matrix& mtr);
+	Matrix(Matrix&& other);
+	Matrix& operator=(Matrix&& other);
+	int& operator()(int row, int col);
+	Matrix operator+(const Matrix& other) const;
+	Matrix operator-(const Matrix& other) const;
+	Matrix operator*(const Matrix& other) const;
+	bool operator==(const Matrix& other) const;
+	friend Matrix transpose(const Matrix& matrix);
+	friend void print(const Matrix& matrix);
+};
+
+#endif // !_MATRIX_H
